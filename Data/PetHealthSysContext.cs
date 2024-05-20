3cb869050c1579aba74a7beed3123719492c93ca
Tuananh4403 <tuananh4403@gmail.com>
2024-05-21 03:33:28 +0700

    create project

---
+++ Data/PetHealthSysContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using PetHealthSys.Entity;
+
+namespace PetHealthSys.Data
+{
+    public class PetHealthSysContext : DbContext
+    {
+        public PetHealthSysContext (DbContextOptions<PetHealthSysContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<PetHealthSys.Entity.User> User { get; set; } = default!;
+    }
+}
